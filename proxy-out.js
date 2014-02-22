741c9181549c87106e2b6d371350d2c0652c4790
Ryan Rowland <Ryan.W.Rowland@gmail.com>
2014-02-21 16:22:54 -0800

    Fixed HTTP

--- proxy-out.js
+++ proxy-out.js
@@ -3,20 +3,19 @@ var http = require('http'),
     url = require('url'),
     tunnel = require('tunnel');
  
-var _httpsAgent, _httpAgent;
+var _httpsAgent, _proxy;
 
 // Our export method simply sets our proxy information
 module.exports = function(proxyUrl) {
     var parsed = url.parse(proxyUrl);
 
-    var proxy = {
+    _proxy = {
       port:     parsed.port     || 80,
       protocol: parsed.protocol || 'http:',
       host:     parsed.hostname
     };
     
-    _httpTunnelingAgent  = tunnel.httpOverHttp({ proxy: proxy })
-    _httpsTunnelingAgent = tunnel.httpsOverHttp({ proxy: proxy })
+    _httpsAgent = tunnel.httpsOverHttp({ proxy: _proxy })
 };
 
 // Helper function to parse a URL string into an options object
@@ -37,30 +36,17 @@ var _parseOptions = function(options) {
   return options;
 };
 
-// Override HTTPS request method
-var _httpsRequest = https.request;
-https.request = function(options, callback) {
-    // Parse destination URL
-    if ('string' === typeof options) {
-      options = _parseOptions(options);
-    }
-
-    options.agent = _httpsTunnelingAgent;
-    return _httpsRequest(options, callback);
+// Helper function to apply proxy settings to an options object
+var _proxifyOptions = function(options) {
+  return {
+    path:     options.protocol + '//' + options.host + options.path,
+    host:     _proxy.host,
+    port:     _proxy.port,
+    protocol: _proxy.protocol
+  };
 };
 
-// Override HTTPS get method
-https.get = function(options, callback) {
-    var req = https.request(options, callback);
-    req.end();
-    return req;
-};
-
-
-// HTTP Requests are currently broken!
-// ----------------------------------
-
-/*
+// Override HTTP request method
 var _httpRequest = http.request;
 http.request = function(options, callback) {
     // Parse destination URL
@@ -70,16 +56,35 @@ http.request = function(options, callback) {
 
     // We don't want to interfere with CONNECT requests
     if (options && options.method !== 'CONNECT') {
-      options.agent = _httpTunnelingAgent;
+      options = _proxifyOptions(options);
     }
 
     return _httpRequest(options, callback);
 };
 
+// Override HTTPS get method
 http.get = function(options, callback) {
     var req = http.request(options, callback);
     req.end();
     return req;
 };
-*/
+
+// Override HTTPS request method
+var _httpsRequest = https.request;
+https.request = function(options, callback) {
+    // Parse destination URL
+    if ('string' === typeof options) {
+      options = _parseOptions(options);
+    }
+
+    options.agent = _httpsAgent;
+    return _httpsRequest(options, callback);
+};
+
+// Override HTTPS get method
+https.get = function(options, callback) {
+    var req = https.request(options, callback);
+    req.end();
+    return req;
+};
 
